695f6cd0ec1398da609246a7e29de549a008711d
yaocheneng <282696845@qq.com>
2019-12-26 23:16:23 +0800

    add Demo

---
+++ Demo/Sources/Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by 姚晨峰 on 2019/12/26.
+//  Copyright © 2019 姚晨峰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- Package.swift
+++ Package.swift
@@ -6,23 +6,13 @@ import PackageDescription
 let package = Package(
     name: "BeeKit",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "BeeKit",
-            targets: ["BeeKit"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .library(name: "BeeKit", targets: ["BeeKit"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "BeeKit",
-            dependencies: []),
-        .testTarget(
-            name: "BeeKitTests",
-            dependencies: ["BeeKit"]),
+        .target(name: "BeeKit", path: "Sources"),
+        .testTarget(name: "BeeKitTests",dependencies: ["BeeKit"]),
+    ],
+    swiftLanguageVersions: [
+        .v5
     ]
 )
