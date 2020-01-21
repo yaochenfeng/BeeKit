b08f6486855f4d8c4ea5691ab1b09c4f9601df9a
yaochenfeng <282696845@qq.com>
2020-01-21 17:02:21 +0800

    add Example for BeeKit

---
+++ Example/Example/Base/NavigationController.swift
@@ -0,0 +1,18 @@
+//
+//  BKNavigationController.swift
+//  Example
+//
+//  Created by yaochenfeng on 2020/1/21.
+//  Copyright © 2020 Example. All rights reserved.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
