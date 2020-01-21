9115c379dee67ee2f6f3ad5ed98341fac911394a
yaochenfeng <282696845@qq.com>
2020-01-21 16:28:31 +0800

    add extension UIViewController

---
+++ Sources/BeeKit/iOS/UIView+bee.swift
@@ -0,0 +1,22 @@
+//
+//  UIView+bee.swift
+//  BeeKit
+//
+//  Created by yaochenfeng on 2020/1/21.
+//
+
+import UIKit
+
+public extension BeeExt where Base: UIView {
+    var controller: UIViewController? {
+        var nextView : UIResponder = self.base
+        while nextView.next != nil {
+            if nextView.next!.isKind(of: UIViewController.self) == true {
+                return nextView.next as? UIViewController
+            } else {
+                nextView = nextView.next!
+            }
+        }
+        return nil;
+    }
+}
