e8d06f4ad2c82a2b58af11edbc44cb25736bc82d
yaochenfeng <282696845@qq.com>
2020-03-19 15:25:24 +0800

    remove constant

--- Sources/BeeKit/Constant.swift
+++
@@ -1,7 +0,0 @@
-public struct Constant {
-    // swiftlint:disable colon force_cast force_unwrapping
-    public static let bundleID:String = Bundle.main.infoDictionary!["CFBundleIdentifier"] as! String
-    public static let version:String = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String
-    public static let buildNum:String = Bundle.main.infoDictionary!["CFBundleVersion"] as! String
-    // swiftlint:enable colon force_cast force_unwrapping
-}
